2f9745982198de05e538f97d45a1baac8b1191bb
Jose Nieves <127802407+jnieves2177@users.noreply.github.com>
2024-04-14 08:13:26 +0000

    fix

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -11,4 +11,4 @@ def np_elementwise(mat1, mat2):
     Return:
         matrix operation
     """
-    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
